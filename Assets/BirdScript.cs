6b9741b4310b87ae734fc75275886c003024ee7c
Putra Alrasy <2019481766@isiswa.uitm.edu.my>
2023-08-03 18:57:43 +0800

    New push

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,18 +7,51 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public float flatStrength;
 
+    // The scale factor to adjust the bird's size
+    public float scaleChangeAmount = 0.1f;
+
+    // The speed at which the bird scales up or down
+    public float scalingSpeed = 2f;
+
+    private Vector3 originalScale;
+    private Vector3 currentScale;
+    private float targetScale = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        // Set the constraints to freeze rotation in the Z-axis (rotation around the forward axis)
+        myRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        originalScale = transform.localScale;
+        currentScale = originalScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.velocity = Vector2.up * flatStrength;
         }
+
+        // Check if 'Q' is held down to start resizing bigger
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ResizeBird(scaleChangeAmount);
+        }
+
+        // Check if 'E' is held down to start resizing smaller
+        if (Input.GetKey(KeyCode.E))
+        {
+            ResizeBird(-scaleChangeAmount);
+        }
+    }
+
+    private void ResizeBird(float scaleChange)
+    {
+        targetScale = Mathf.Clamp(targetScale + scaleChange, 0.5f, 2f);
+        currentScale.x = Mathf.MoveTowards(currentScale.x, targetScale, Time.deltaTime * scalingSpeed);
+        currentScale.y = Mathf.MoveTowards(currentScale.y, targetScale, Time.deltaTime * scalingSpeed);
+        transform.localScale = currentScale;
     }
 }
